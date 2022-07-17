93ccde39f9c46fae844a7b43f7552e0314a03b35
Vinaya Srri Dasari <89027047+VinayaSrriD@users.noreply.github.com>
2022-07-17 15:39:43 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+let  myLeads = []
+let inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+
+
+if(leadsFromLocalStorage)
+{
+    myLeads = leadsFromLocalStorage
+    renderLeads(myLeads)
+}
+
+function renderLeads(leads){
+    let listItems = ""
+    for(let i=0; i < leads.length; i++){
+    listItems += `
+        <li> 
+            <a target = '_blank' href = '${leads[i]}'> ${leads[i]} </a>
+        </li>`
+    }
+    ulEl.innerHTML = listItems
+}
+
+inputBtn.addEventListener("click" , function(){
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+    renderLeads(myLeads)
+})
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    renderLeads(myLeads)
+})
+
+tabBtn.addEventListener("click", function(){
+        chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+                myLeads.push(tabs[0].url);
+                localStorage.setItem("myLeads", JSON.stringify(myLeads))
+                renderLeads(myLeads)
+    })
+})
